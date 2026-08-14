02b305f4518d980100dfac1ac0b551e3fdac2c3e
Arnold-JrIM <69058414+Arnold-JrIM@users.noreply.github.com>
2026-08-14 18:06:28 -0300

    feat: expose T03 to T05 runners

--- src/cpgf/trails/__init__.py
+++ src/cpgf/trails/__init__.py
@@ -1,4 +1,7 @@
 from .t01_weekend import run_t01
 from .t02_installment import run_t02
+from .t03_exact_repetition import run_t03
+from .t04_multi_cardholder import run_t04
+from .t05_vendor_recurrence import run_t05
 
-__all__ = ["run_t01", "run_t02"]
+__all__ = ["run_t01", "run_t02", "run_t03", "run_t04", "run_t05"]
