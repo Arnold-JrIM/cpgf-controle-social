64f13cb27e3c5bdff136962808fff470facf8ecb
Arnold-JrIM <69058414+Arnold-JrIM@users.noreply.github.com>
2026-08-14 23:09:24 -0300

    serving: expõe contrato de distribuição

--- src/cpgf/serving/__init__.py
+++ src/cpgf/serving/__init__.py
@@ -1,3 +1,11 @@
+from .distribution import (
+    DEFAULT_CHECKSUM_URL,
+    DEFAULT_RELEASE_URL,
+    ServingBootstrapResult,
+    ServingDistributionConfig,
+    ServingUnavailableError,
+    bootstrap_serving,
+)
 from .duckdb import build_duckdb_catalog, catalog_metadata, open_catalog
 from .materialize import (
     build_diagnostic_serving_tables,
@@ -10,8 +18,14 @@ from .repository import ServingRepository
 from .views import authorized_views_from_manifest
 
 __all__ = [
+    "DEFAULT_CHECKSUM_URL",
+    "DEFAULT_RELEASE_URL",
+    "ServingBootstrapResult",
+    "ServingDistributionConfig",
     "ServingRepository",
+    "ServingUnavailableError",
     "authorized_views_from_manifest",
+    "bootstrap_serving",
     "build_diagnostic_serving_tables",
     "build_duckdb_catalog",
     "build_serving_bundle",
